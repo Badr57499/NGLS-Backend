e16dab6b3b81d6d3bb0a23b5a54c40c0d24f8745
Badr57499 <bs0027532@gmail.com>
2026-08-01 21:25:20 +0300

    Refactor server.js for improved readability and structure

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const connectDb = require('./Config/connect');
 require('dotenv').config();
+
 const { setServers } = require('node:dns/promises');
 setServers(['1.1.1.1', '8.8.8.8']);
+
+// Allow all origins
+app.use(cors());
+
+// Handle preflight requests
+app.options('*', cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 
 app.use('/uploads', express.static('uploads'));
 app.use('/api', require('./Routes/registerRoute'));
@@ -18,16 +25,13 @@ app.use('/api', require('./Routes/loginRoute'));
 app.use('/api', require('./Routes/ancsRoute'));
 app.use('/api', require('./Routes/videosRoute'));
 
-
-
 async function StartServer() {
-    try{
+    try {
         await connectDb();
         app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+            console.log(`Server is running on port ${port}`);
         });
-        
-    }catch(error){
+    } catch (error) {
         console.error('Failed to start server:', error.message);
         process.exit(1);
     }
