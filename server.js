05fb0a074d001c2bb4e941d179c0f6912bee16d5
Badr57499 <bs0027532@gmail.com>
2026-08-01 21:21:52 +0300

    Remove CORS configuration from server.js
    Removed CORS middleware and related headers from server.js.

--- server.js
+++ server.js
@@ -2,24 +2,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require('cors');
+
 const connectDb = require('./Config/connect');
 require('dotenv').config();
 const { setServers } = require('node:dns/promises');
 setServers(['1.1.1.1', '8.8.8.8']);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.options('*', cors());
 
-// Allow all origins explicitly for CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    if (req.method === 'OPTIONS') return res.sendStatus(200);
-    next();
-});
 
 
 app.use('/uploads', express.static('uploads'));
